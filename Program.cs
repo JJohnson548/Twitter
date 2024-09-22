232be15d9977ff2d796a8e44350f7a05e7dcb94f
johns <johns@JONSCOMPUTER>
2024-09-22 15:28:45 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Twitter
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //promting user for their message
+            Console.WriteLine("Create a new message");
+
+            string userMessage = Console.ReadLine();
+
+            //getting length of user message
+            int userMessageLength = userMessage.Length;
+
+            //evaluating if message can be posted
+            if (userMessageLength <= 140)
+            {
+
+                Console.WriteLine("\nPosted");
+            }
+            else { 
+                
+                Console.WriteLine("\nRejected");
+              
+      
+            }
+
+        }
+    }
+}
